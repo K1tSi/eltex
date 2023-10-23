2755e5606c48f147856e523451261ff3139eec57
Nikita Sizykh <nikitasizyh03@gmail.com>
2023-10-23 16:34:10 +0700

    +updateValueForArray (Осталось только меню пользователя)

--- Module_2/Day_2/2.1/main.c
+++ Module_2/Day_2/2.1/main.c
@@ -63,72 +63,89 @@ int addValueForArray(char** array, char* newValue, int numberVal, int lenPole, i
     strncpy(array[numberVal], newValue, lenPole);
     }
 }
-int updateContact(Contact* contacts, char* str, char whoUpdate, int num_contact){
+int updateValueForArray(Contact* contacts, char* str, char whatUpdate, int num_contact, int num_change){
     int countNotNull = 0;
-    if(whoUpdate == 'a'){
-    strncpy(contacts[num_contact].firstName, str, LEN_NAME);
-    } else if(whoUpdate == 'b'){
-    strncpy(contacts[num_contact].lastName, str, LEN_NAME);
-    } else if(whoUpdate == 'c'){
-    strncpy(contacts[num_contact].fatherName, str, LEN_NAME);
-    } else if(whoUpdate == 'd'){
-    strncpy(contacts[num_contact].placeOfWork, str, LEN_WORK);
-    } else if(whoUpdate == 'e'){
-    strncpy(contacts[num_contact].jobTitle, str, LEN_WORK);
-    } else if(whoUpdate == 'f'){
-    for(; strlen(contacts[num_contact].phoneNumbers[countNotNull])!=0; countNotNull++);
-    if(countNotNull>=COUNT_OF_PHONE-1) return -2;
-    strncpy(contacts[num_contact].phoneNumbers[countNotNull], str, LEN_PHONE);
-    } else if(whoUpdate == 'g'){
-    for(; strlen(contacts[num_contact].emails[countNotNull])!=0; countNotNull++);
-    if(countNotNull>=COUNT_OF_EMAIL-1) return -3;
-    strncpy(contacts[num_contact].emails[countNotNull], str, LEN_EMAIL);
-    //addValueForArray(contacts[num_contact].emails, str, -1, LEN_EMAIL, COUNT_OF_EMAIL);
-    } else if(whoUpdate == 'j'){
-    for(; strlen(contacts[num_contact].linksToSocialNetwork[countNotNull])!=0; countNotNull++);
-    if(countNotNull>=COUNT_OF_SOCIAL_NETWORK-1) return -4;
-    strncpy(contacts[num_contact].linksToSocialNetwork[countNotNull], str, LEN_EMAIL);
-    //addValueForArray(contacts[num_contact].linksToSocialNetwork, str, -1, LEN_LINK, COUNT_OF_SOCIAL_NETWORK);
-    } else if(whoUpdate == 'k'){
-    for(; strlen(contacts[num_contact].messengers[countNotNull])!=0; countNotNull++);
-    if(countNotNull>=COUNT_OF_MESSENGER-1) return -5;
-    strncpy(contacts[num_contact].messengers[countNotNull], str, LEN_MESSENGER);
-    //addValueForArray(contacts[num_contact].messengers, str, -1, LEN_MESSENGER, COUNT_OF_MESSENGER);
+    if(whatUpdate == 'f'){
+        for(; strlen(contacts[num_contact].phoneNumbers[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_PHONE-1 || num_change >= countNotNull) return -2;
+        strncpy(contacts[num_contact].phoneNumbers[num_change], str, LEN_PHONE);
+    }else if(whatUpdate == 'g'){
+        for(; strlen(contacts[num_contact].emails[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_EMAIL-1 || num_change >= countNotNull) return -3;
+        strncpy(contacts[num_contact].emails[num_change], str, LEN_EMAIL);
+    } else if(whatUpdate == 'j'){
+        for(; strlen(contacts[num_contact].linksToSocialNetwork[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_SOCIAL_NETWORK-1|| num_change >= countNotNull) return -4;
+        strncpy(contacts[num_contact].linksToSocialNetwork[num_change], str, LEN_LINK);
+    } else if(whatUpdate == 'k'){
+        for(; strlen(contacts[num_contact].messengers[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_MESSENGER-1 || num_change >= countNotNull) return -5;
+        strncpy(contacts[num_contact].messengers[num_change], str, LEN_MESSENGER);
+    }
+}
+int updateContact(Contact* contacts, char* str, char whatUpdate, int num_contact){
+    int countNotNull = 0;
+    if(whatUpdate == 'a'){
+        strncpy(contacts[num_contact].firstName, str, LEN_NAME);
+    } else if(whatUpdate == 'b'){
+        strncpy(contacts[num_contact].lastName, str, LEN_NAME);
+    } else if(whatUpdate == 'c'){
+        strncpy(contacts[num_contact].fatherName, str, LEN_NAME);
+    } else if(whatUpdate == 'd'){
+        strncpy(contacts[num_contact].placeOfWork, str, LEN_WORK);
+    } else if(whatUpdate == 'e'){
+        strncpy(contacts[num_contact].jobTitle, str, LEN_WORK);
+    } else if(whatUpdate == 'f'){
+        for(; strlen(contacts[num_contact].phoneNumbers[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_PHONE-1) return -2;
+        strncpy(contacts[num_contact].phoneNumbers[countNotNull], str, LEN_PHONE);
+    } else if(whatUpdate == 'g'){
+        for(; strlen(contacts[num_contact].emails[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_EMAIL-1) return -3;
+        strncpy(contacts[num_contact].emails[countNotNull], str, LEN_EMAIL);
+    } else if(whatUpdate == 'j'){
+        for(; strlen(contacts[num_contact].linksToSocialNetwork[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_SOCIAL_NETWORK-1) return -4;
+        strncpy(contacts[num_contact].linksToSocialNetwork[countNotNull], str, LEN_LINK);
+    } else if(whatUpdate == 'k'){
+        for(; strlen(contacts[num_contact].messengers[countNotNull])!=0; countNotNull++);
+        if(countNotNull>=COUNT_OF_MESSENGER-1) return -5;
+        strncpy(contacts[num_contact].messengers[countNotNull], str, LEN_MESSENGER);
     }else return -1;
     return 0;
 }
 
 int deleteContact(Contact* contacts, int id, int* count_contacts){
-if(id < *count_contacts){
-    for(int i = id; i < (*count_contacts)-1; i++){
-        contacts[i] = contacts[i+1];
-        contacts[i].id = i;
-        }
-    (*count_contacts)--;
-    return 0;
-   }
-   else return -1;
+    if(id < *count_contacts){
+        for(int i = id; i < (*count_contacts)-1; i++){
+            contacts[i] = contacts[i+1];
+            contacts[i].id = i;
+            }
+        (*count_contacts)--;
+        return 0;
+    }
+    else return -1;
 }
 void printContactsAllInfo(Contact* contacts, int count_contacts) 
 {
-int cPhone = 0, cEmail = 0, cLink = 0, cMessenger = 0, countNotNull = 1;
- printf("ID\t|\tИмя\t|\tФамилия\t|\tОтчество\t|\tМесто_работы\t|\tДолжность\t|\tТелефон\t|\tЭл.почта\t|\tСоцсеть\t|\tМессенджер |\n");
- for(int i = 0; i<count_contacts; i++){
-     printf("%d\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", contacts[i].id,
-    contacts[i].firstName, contacts[i].lastName, contacts[i].fatherName,
-    contacts[i].placeOfWork, contacts[i].jobTitle,
-    contacts[i].phoneNumbers[0], contacts[i].emails[0],
-    contacts[i].linksToSocialNetwork[0], contacts[i].messengers[0]);
-    
-    for(; strlen(contacts[i].messengers[countNotNull])!=0 || strlen(contacts[i].phoneNumbers[countNotNull])!=0 || strlen(contacts[i].linksToSocialNetwork[countNotNull]) !=0 || strlen(contacts[i].emails[countNotNull])!=0; countNotNull++) printf("\t\t\t\t\t\t\t\t\t\t\t\t%d.\t\t%s\t\t%s\t\t%s\t\t%s\n", countNotNull, 
-    contacts[i].phoneNumbers[countNotNull], contacts[i].emails[countNotNull],
-    contacts[i].linksToSocialNetwork[countNotNull], contacts[i].messengers[countNotNull]);
-    countNotNull = 1;
-    //for(; strlen(contacts[i].messengers[cEmail])!=0; cEmail++);
-    //for(; strlen(contacts[i].messengers[cLink])!=0; countNotNull++);
-    //for(; strlen(contacts[i].messengers[cMessenger])!=0; countNotNull++);
-    
-    //if(strlen())
+    int cPhone = 0, cEmail = 0, cLink = 0, cMessenger = 0, countNotNull = 1;
+    printf("ID\t|\tИмя\t|\tФамилия\t|\tОтчество\t|\tМесто_работы\t|\tДолжность\t|\tТелефон\t|\tЭл.почта\t|\tСоцсеть\t|\tМессенджер |\n");
+    for(int i = 0; i<count_contacts; i++){
+        printf("%d\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", contacts[i].id,
+        contacts[i].firstName, contacts[i].lastName, contacts[i].fatherName,
+        contacts[i].placeOfWork, contacts[i].jobTitle,
+        contacts[i].phoneNumbers[0], contacts[i].emails[0],
+        contacts[i].linksToSocialNetwork[0], contacts[i].messengers[0]);
+        
+        for(; strlen(contacts[i].messengers[countNotNull])!=0 || strlen(contacts[i].phoneNumbers[countNotNull])!=0 || strlen(contacts[i].linksToSocialNetwork[countNotNull]) !=0 || strlen(contacts[i].emails[countNotNull])!=0; countNotNull++) printf("\t\t\t\t\t\t\t\t\t\t\t\t%d.\t\t%s\t\t%s\t\t%s\t\t%s\n", countNotNull, 
+        contacts[i].phoneNumbers[countNotNull], contacts[i].emails[countNotNull],
+        contacts[i].linksToSocialNetwork[countNotNull], contacts[i].messengers[countNotNull]);
+        countNotNull = 1;
+        //for(; strlen(contacts[i].messengers[cEmail])!=0; cEmail++);
+        //for(; strlen(contacts[i].messengers[cLink])!=0; countNotNull++);
+        //for(; strlen(contacts[i].messengers[cMessenger])!=0; countNotNull++);
+        
+        //if(strlen())
     }
 }
 void printContactsSmall(Contact* contacts, int count_contacts) 
@@ -145,6 +162,7 @@ int main()
     Contact contacts[MAX_SIZE];
     int count_contacts = 0;
     int* pcount = &count_contacts;
+    char operation;
     addNewContact(contacts, "Nikita", "Sizykh", pcount);
     addNewContact(contacts, "Nikita", "Anisimov", pcount);
     updateContact(contacts,"Kirilllll", 'b', 1);
@@ -162,8 +180,20 @@ int main()
     updateContact(contacts, "nik.nik@gmail.ru", 'g',0);
     updateContact(contacts, "nikittta@mail.ru", 'g',0);
     updateContact(contacts, "+7981232453", 'f',0);
-        updateContact(contacts, "+79832432423", 'f',0);
+    
+    updateContact(contacts, "+79832432423", 'f',0);
+
+    updateValueForArray(contacts, "+7xxxxxx",'f',0,1);
     printContactsAllInfo(contacts, count_contacts);
     
-
+    while(1) {
+        //system("clear");
+        
+        printf("Список контактов:\n");
+        printContactsSmall(contacts, count_contacts);
+        scanf("%c", &operation);
+        //fgets(operation, 2, stdin);
+        printf("%c",operation);
+    }
+    
 }
